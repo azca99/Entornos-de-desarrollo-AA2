2fefe1391ea102f0885e737fd989f04e7834b643
Daniel Azcárate <azca99@gmail.com>
2026-04-12 16:33:20 +0100

    Listar equipos y detalles en index y añadir botones

---
+++ frontend/src/index.js
@@ -0,0 +1,56 @@
+import axios from 'https://cdn.jsdelivr.net/npm/axios@1.6.8/+esm';
+import { el, icon, td } from './documentUtil.js';
+
+window.readTeams = function () {
+    axios.get('http://localhost:3000/teams')
+        .then((response) => {
+            const teamList = response.data;
+            const teamTable = el('tableBody');
+
+            teamTable.innerHTML = '';
+
+            teamList.forEach(team => {
+                const row = document.createElement('tr');
+                row.id = 'team-' + team.id;
+
+                row.innerHTML =
+                    td(team.name) +
+                    td(team.city) +
+                    td(team.sport) +
+                    '<td>' +
+                    '<a class="btn btn-warning me-1" href="modify.html?id=' + team.id + '">' +
+                    icon('edit') +
+                    '</a>' +
+                    '<a class="btn btn-danger" href="javascript:removeTeam(' + team.id + ')">' +
+                    icon('delete') +
+                    '</a>' +
+                    '</td>';
+
+                teamTable.appendChild(row);
+            });
+        })
+        .catch((error) => {
+            console.error('Error al cargar los equipos:', error);
+        });
+};
+
+window.removeTeam = function (id) {
+    if (confirm('¿Estás seguro de que deseas eliminar este equipo?')) {
+        axios.delete('http://localhost:3000/teams/' + id)
+            .then((response) => {
+                if (response.status == 204) {
+                    notifyOk('Ciudad eliminada correctamente');
+                    el('city-' + id).remove();
+                }
+            })
+            .catch((error) => {
+                console.error('Error al eliminar el equipo:', error);
+
+                if (error.response && error.response.data && error.response.data.message) {
+                    alert(error.response.data.message);
+                } else {
+                    alert('Error al eliminar el equipo. Por favor, inténtalo de nuevo.');
+                }
+            });
+    }
+};
